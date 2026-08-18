46dca3036fa6db28e21db2ebb0d9359e32a16c3b
Usama Khan <usamakhan58461@gmail.com>
2026-08-18 17:19:17 +0500

    remove

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import './App.css'
-// import BgVideo from '../public/videos/video.mp4';
-import Login from './component/login/Loginpage';
-import SignUp from './component/signUp/SignUpPage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import AllComp from './AllComp';
-;
+
 function App() {
   return (
     <div>
       <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      {/* <video
-        className="absolute top-0 bottom-0 left-0  object-cover "
-        id='background'
-        autoPlay
-        loop
-        muted
-        >
-        <source src={BgVideo} type="video/mp4" />
-      </video> */}
-      <Route path='/home' element={<AllComp/>}/>
+        <Route path='/' element={<AllComp />} />
+        <Route path='/home' element={<AllComp />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </div>
   )
